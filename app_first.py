1906f523ee90fadd533490c9aa1cdb7afc270500
unknown <jim80129@gmail.com>
2020-09-10 21:14:48 +0800

    make it better

--- app_first.py
+++ app_first.py
@@ -1,6 +1,6 @@
 #真正讓網站上線
 # 除了這個python檔，還要建立3個檔
-#  1.runtim.txt 寫python的版本(Python-3.8.2)
+#  1.runtim.txt 寫python的版本(Python-3.8.5)
 #  2.requirements.txt 寫使用的套件=>(Flask=>程式中用的，gunicorn=>雲端運作用的)
 #  3.Procfile 管理heroku如何執行程式(web gunicorn python檔名:python程式檔中實體物件)
 #  4.安裝git.tool(已經安裝過就Ok了，如果沒有就去"https://git-scm.com/download/win"安裝)
@@ -9,6 +9,8 @@
 #  7.安裝heroku CLI 
 #  8.在下方終端機鍵入git，以及鍵入heroku會出現一堆東西，代表成功安裝了
 #  9.鍵入heroku login，打上帳號密碼登入
+#  10.初始化專案(只需要做一次，後續更新不需要做): 1.鍵入git init 2.鍵入heroku git:remote -a python-trainingfu
+#  11.布署專案: 1.鍵入git add . 2.
 from flask import Flask
 app_1=Flask(__name__)
 
